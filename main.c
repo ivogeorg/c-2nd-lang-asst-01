d6fd0fae5a8ef1075613e3e05f096fb8d90473f2
Ivo Georgiev <ivogeorg@gmail.com>
2019-02-27 09:24:27 -0700

    Added a new source file pointers.c to show how to organize project with multiple source files. Added in main.c a forward declaration of a function defined in another source file, but called from main().

--- main.c
+++ main.c
@@ -4,8 +4,26 @@
 
 #include <stdio.h>
 
+
+// Forward declarations
+// ====================
+// Functions that are called from (the functions)
+// in this file have to have forward declarations,
+// which are just the function signatures (their
+// first lines, terminated with a trailing semicolon.
+// The definitions of functions of these functions,
+// that is, the full signatures with code blocks,
+// may be in other source files (files with the .c
+// extension) in the same project. The compiler
+// needs the declarations and the linker needs
+// the definitions.
+void some_pointer_play_function();
+
+
 int main() {
     printf("Hello, MSUD!\n");
+
+    some_pointer_play_function();
 
     return 0;
 }
---
+++ pointers.c
@@ -0,0 +1,14 @@
+//
+// Created by Ivo Georgiev on 2019-02-26.
+//
+// A sample source file defining a function
+// that will be called in main() in file
+// main.c. See the explanatory comment at
+// the beginning of main.c.
+//
+
+#include <stdio.h>
+
+void some_pointer_play_function() {
+    printf("some_pointer_play_function executed...\n");
+}
